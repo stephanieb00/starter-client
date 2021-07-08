df9fd96516b462c11c4368114f0488d0cd7fa655
Stephanie Bravo <stephanieb@tywls-astoria.org>
2021-07-08 00:31:52 -0400

    Created and implemented Single campus view

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -1,22 +1,44 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 
 const CampusView = (props) => {
   const {campus} = props;
   return (
-    <div>      
-      <h1>{campus.name}</h1>
-      <p>{campus.description}</p>
-      <ul>
-      {campus.students.map( student => {
-        let name = student.firstname + " " + student.lastname;
-        return (
-          <li key={student.id}>{name}</li>
-        );
-      })}
-      </ul>
+    <div>   
+      <div className="pageLinks">
+          <Link to="/"> Home</Link>
+          <Link to="/students">Students</Link>
+          <Link to="/campuses">Campuses</Link>
+      </div>  
+
+      <div className="container">
+        <h1>{campus.name}</h1>
+        <img classname="objectImage" src={campus.imageUrl} />
+        <p>{campus.description}</p>
+        <p>{campus.address}</p>
+
+        <ul>
+        {campus.students.map( student => {
+          let name = student.firstname + " " + student.lastname;
+          return (
+            <div className="studentLink" key={student.id}>
+               <Link to={`/student/${student.id}`}>
+                <li>{name}</li>
+              </Link>
+            </div>
+            
+          );
+        })}
+        </ul>
+      </div>
     </div>
   );
 
+};
+
+CampusView.propTypes = {
+  allStudents: PropTypes.array.isRequired,
 };
 
 export default CampusView;
